16fea6e3fa34055f96f52e51983f0ea31c444e8e
Roman Peresoliak <r.peresolyak@gmail.com>
2016-09-19 10:44:49 +0300

    updated

--- scripts/scrapyd_run.py
+++ scripts/scrapyd_run.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-
 from twisted.scripts.twistd import run
 from os.path import join, dirname
-from sys import argv
-import scrapyd
+from sys import argv, path
 
+
+# FIXME only for local usage
+module_path = "/home/roman/PycharmProjects/scrapyd_fork/scrapyd"
+if module_path not in path:
+    path.append(module_path)
+# FIXME
+
+import scrapyd
 
 def main():
     argv[1:1] = ['-n', '-y', join(dirname(scrapyd.__file__), 'txapp.py')]
